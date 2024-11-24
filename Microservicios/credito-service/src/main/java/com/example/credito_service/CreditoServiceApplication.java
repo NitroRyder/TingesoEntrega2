68ad768446a0c7e1643ca7d7c88f29524105ef02
NitroRyder <benjamin.valenzuela.p@usach.cl>
2024-11-24 20:58:12 -0300

    REPARACIONES

--- Microservicios/credito-service/src/main/java/com/example/credito_service/CreditoServiceApplication.java
+++ Microservicios/credito-service/src/main/java/com/example/credito_service/CreditoServiceApplication.java
@@ -2,8 +2,10 @@ package com.example.credito_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class CreditoServiceApplication {
 
 	public static void main(String[] args) {
