8bee7ce53588fb358913ab6bfcd90242e7b94b17
NitroRyder <benjamin.valenzuela.p@usach.cl>
2024-11-28 10:21:18 -0300

    RE-EMLIMINACIÓN

--- Microservicios/usuario-service/src/main/java/com/example/usuario_service/UsuarioServiceApplication.java
+++ Microservicios/usuario-service/src/main/java/com/example/usuario_service/UsuarioServiceApplication.java
@@ -3,8 +3,6 @@ package com.example.usuario_service;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableDiscoveryClient
@@ -13,10 +11,5 @@ public class UsuarioServiceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(UsuarioServiceApplication.class, args);
 	}
-	@Bean
-	public TomcatServletWebServerFactory servletContainer() {
-		TomcatServletWebServerFactory factory = new TomcatServletWebServerFactory();
-		factory.setPort(8084); // Change the port here
-		return factory;
-	}
+
 }
