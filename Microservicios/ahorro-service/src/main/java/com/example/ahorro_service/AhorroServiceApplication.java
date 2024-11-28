a9d7c2fc92f0733cf0c48008e2dcba484b913222
NitroRyder <benjamin.valenzuela.p@usach.cl>
2024-11-28 10:09:27 -0300

    Eliminación de puertos

--- Microservicios/ahorro-service/src/main/java/com/example/ahorro_service/AhorroServiceApplication.java
+++ Microservicios/ahorro-service/src/main/java/com/example/ahorro_service/AhorroServiceApplication.java
@@ -2,19 +2,14 @@ package com.example.ahorro_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 public class AhorroServiceApplication {
 
-	public static void main(String[] args) {SpringApplication.run(AhorroServiceApplication.class, args);}
-	@Bean
-	public TomcatServletWebServerFactory servletContainer() {
-		TomcatServletWebServerFactory factory = new TomcatServletWebServerFactory();
-		factory.setPort(8082); // Change the port here
-		return factory;
+	public static void main(String[] args) {
+		SpringApplication.run(AhorroServiceApplication.class, args);
 	}
+
 }
--- Microservicios/credito-service/src/main/java/com/example/credito_service/CreditoServiceApplication.java
+++ Microservicios/credito-service/src/main/java/com/example/credito_service/CreditoServiceApplication.java
@@ -3,8 +3,6 @@ package com.example.credito_service;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableDiscoveryClient
@@ -13,10 +11,5 @@ public class CreditoServiceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(CreditoServiceApplication.class, args);
 	}
-	@Bean
-	public TomcatServletWebServerFactory servletContainer() {
-		TomcatServletWebServerFactory factory = new TomcatServletWebServerFactory();
-		factory.setPort(8083); // Change the port here
-		return factory;
-	}
+
 }
