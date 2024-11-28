7bf6b0dd57cfb171cad617b66578ffe152991b6d
NitroRyder <benjamin.valenzuela.p@usach.cl>
2024-11-28 02:17:52 -0300

    PUERTOS PARA FUNCIONES

--- Microservicios/evalua-service/src/main/java/com/example/evalua_service/EvaluaServiceApplication.java
+++ Microservicios/evalua-service/src/main/java/com/example/evalua_service/EvaluaServiceApplication.java
@@ -3,6 +3,8 @@ package com.example.evalua_service;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableDiscoveryClient
@@ -11,5 +13,10 @@ public class EvaluaServiceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(EvaluaServiceApplication.class, args);
 	}
-
+	@Bean
+	public TomcatServletWebServerFactory servletContainer() {
+		TomcatServletWebServerFactory factory = new TomcatServletWebServerFactory();
+		factory.setPort(8086); // Change the port here
+		return factory;
+	}
 }
