90938bf6930de207bfa37d2955c7950fac4a49f7
NitroRyder <benjamin.valenzuela.p@usach.cl>
2024-11-29 19:32:56 -0300

    Cambios de model de USUARIO

--- Microservicios/usuario-service/src/main/java/com/example/usuario_service/model/Credito.java
+++ Microservicios/usuario-service/src/main/java/com/example/usuario_service/model/Credito.java
@@ -1,5 +1,6 @@
 package com.example.usuario_service.model;
 
+import jakarta.persistence.Lob;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -15,13 +16,21 @@ public class Credito {
     private double segudesg;
     private double seguince;
     private double comiad;
+    @Lob
     private byte[] comprobanteIngresos;
+    @Lob
     private byte[] certificadoAvaluo;
+    @Lob
     private byte[] historialCrediticio;
+    @Lob
     private byte[] escrituraPrimeraVivienda;
+    @Lob
     private byte[] planNegocios;
+    @Lob
     private byte[] estadosFinancieros;
+    @Lob
     private byte[] presupuestoRemodelacion;
+    @Lob
     private byte[] dicom;
     private String state;
     private int usuarioId;
