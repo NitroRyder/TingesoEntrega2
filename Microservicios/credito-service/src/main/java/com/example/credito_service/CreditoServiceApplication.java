b107409e2eb502fb0cc9d31b8a1cd9968e6a4d10
NitroRyder <benjamin.valenzuela.p@usach.cl>
2024-11-30 15:05:37 -0300

    CAMBIOS EN SERVICE APPLICATIOSN

--- Microservicios/credito-service/src/main/java/com/example/credito_service/CreditoServiceApplication.java
+++ Microservicios/credito-service/src/main/java/com/example/credito_service/CreditoServiceApplication.java
@@ -2,9 +2,7 @@ package com.example.credito_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableDiscoveryClient
@@ -12,11 +10,5 @@ public class CreditoServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CreditoServiceApplication.class, args);
-	}
-	@Bean
-	public TomcatServletWebServerFactory servletContainer() {
-		TomcatServletWebServerFactory factory = new TomcatServletWebServerFactory();
-		factory.setPort(8020); // Change the port here
-		return factory;
 	}
 }
--- Microservicios/evalua-service/src/main/java/com/example/evalua_service/EvaluaServiceApplication.java
+++ Microservicios/evalua-service/src/main/java/com/example/evalua_service/EvaluaServiceApplication.java
@@ -3,8 +3,6 @@ package com.example.evalua_service;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableDiscoveryClient
@@ -12,11 +10,5 @@ public class EvaluaServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EvaluaServiceApplication.class, args);
-	}
-	@Bean
-	public TomcatServletWebServerFactory servletContainer() {
-		TomcatServletWebServerFactory factory = new TomcatServletWebServerFactory();
-		factory.setPort(8086); // Change the port here
-		return factory;
 	}
 }
