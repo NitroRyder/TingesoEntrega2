5a353693c01ecc928c787111e6d8422b9e2fbbf2
NitroRyder <benjamin.valenzuela.p@usach.cl>
2024-11-30 02:32:50 -0300

    revición de Seguimiento

--- Microservicios/seguimiento-service/src/main/java/com/example/seguimiento_service/service/SeguimientoService.java
+++ Microservicios/seguimiento-service/src/main/java/com/example/seguimiento_service/service/SeguimientoService.java
@@ -8,21 +8,22 @@ import org.springframework.web.client.RestTemplate;
 @Service
 public class SeguimientoService {
 
-    RestTemplate restTemplate;
+    private final RestTemplate restTemplate;
+
+    public SeguimientoService() {
+        this.restTemplate = new RestTemplate();
+    }
     //----------------------[P5]- FUNCIONES DE SEGUIMENTO ---------------------------------------//
     // + SEQUIMIENTO DEL ESTADO DE LA SOLICITUD POR ID DEL USUARIO:
     public Credito followCredito(Long userId, Long creditId) {
-        // Fetch the Usuario object using RestTemplate
-        Usuario usuario = restTemplate.getForObject("http://usuario-service/usuario/" + userId, Usuario.class); // OBTENCIÓN DE USUARIO COMPLETO POR ID
+        Usuario usuario = restTemplate.getForObject("http://usuario-service/usuario/" + userId, Usuario.class);
         if (usuario == null) {
             throw new IllegalArgumentException("ERROR: USUARIO NO ENCONTRADO");
         }
-        // Fetch the Credito object using RestTemplate
-        Credito solicitud = restTemplate.getForObject("http://credito-service/credito/byusuario/" + creditId, Credito.class); // OBTENGO LA SOLICITUD DEL USUARIO -> PARA EL RETORNO DE ARCHIVOS
+        Credito solicitud = restTemplate.getForObject("http://credito-service/credito/byusuario/" + creditId, Credito.class);
         if (solicitud == null) {
             throw new IllegalArgumentException("ERROR: SOLICITUD NO ENCONTRADA");
         }
-        //-------------------------------------------------------------------------//
         return solicitud;
     }
 }
