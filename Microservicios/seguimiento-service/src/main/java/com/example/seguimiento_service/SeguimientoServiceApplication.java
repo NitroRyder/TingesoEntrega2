77afce5ff5ea2a7d63a8ded6f6322dd92f8d4cc4
NitroRyder <benjamin.valenzuela.p@usach.cl>
2024-11-30 15:05:23 -0300

    CAMBIOS EN SERVICE APPLICATIONS

--- Microservicios/seguimiento-service/src/main/java/com/example/seguimiento_service/SeguimientoServiceApplication.java
+++ Microservicios/seguimiento-service/src/main/java/com/example/seguimiento_service/SeguimientoServiceApplication.java
@@ -2,19 +2,13 @@ package com.example.seguimiento_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.client.RestTemplate;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class SeguimientoServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SeguimientoServiceApplication.class, args);
-	}
-
-	@Bean
-	public RestTemplate restTemplate() {
-		return new RestTemplate();
 	}
 }
--- Microservicios/total-service/src/main/java/com/example/total_service/TotalServiceApplication.java
+++ Microservicios/total-service/src/main/java/com/example/total_service/TotalServiceApplication.java
@@ -2,19 +2,13 @@ package com.example.total_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
-import org.springframework.context.annotation.Bean;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class TotalServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TotalServiceApplication.class, args);
-	}
-	@Bean
-	public TomcatServletWebServerFactory servletContainer() {
-		TomcatServletWebServerFactory factory = new TomcatServletWebServerFactory();
-		factory.setPort(8088); // Change the port here
-		return factory;
 	}
 }
