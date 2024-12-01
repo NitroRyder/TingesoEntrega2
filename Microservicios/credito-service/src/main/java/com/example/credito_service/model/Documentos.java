90e06bf99f917a5ece3ff2599d31f0506a46cb7b
NitroRyder <benjamin.valenzuela.p@usach.cl>
2024-11-30 22:27:03 -0300

    modelo de Documentos en CREDITOS

---
+++ Microservicios/credito-service/src/main/java/com/example/credito_service/model/Documentos.java
@@ -0,0 +1,13 @@
+package com.example.credito_service.model;
+
+import jakarta.persistence.Lob;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+public class Documentos {
+    private int id;
+    private int creditoId;
+    @Lob
+    private byte[] documento;
+}
