0edb7c6a918ad797cc7d435f82fa8ee28d3cb904
NitroRyder <benjamin.valenzuela.p@usach.cl>
2024-12-02 21:33:44 -0300

    Cierre de simulación

--- Microservicios/front-end/src/App.jsx
+++ Microservicios/front-end/src/App.jsx
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-d
 import Home from './components/Home';
 import ClientHome from './components/ClientHome';
 import RegisterUsuario from './components/RegisterUsuario';
-{/*import SimularCredito from './components/SimularCredito';*/}
+import SimularCredito from './components/SimularCredito';
 {/*import SolicitarPrestamo from './components/SolicitarPrestamo';*/}
 {/*import EvaluarCredito from './components/EvaluarCredito';*/}
 {/*import CalcularCosto from './components/CalcularCosto';*/}
@@ -23,8 +23,8 @@ function App() {
           <Route path="/" element={<Home/>} />
           <Route path="/home/Client" element={<ClientHome />}/>
           <Route path="/usuario/register" element={<RegisterUsuario />} />
-          {/*<Route path="/credito/simular" element={<SimularCredito />} />
-          <Route path="/credito/solicitar" element={<SolicitarPrestamo />} />
+          <Route path="/credito/simular" element={<SimularCredito />} />
+          {/*<Route path="/credito/solicitar" element={<SolicitarPrestamo />} />
           <Route path="/credito/evaluar" element={<EvaluarCredito />} />
           <Route path="/credito/seguimiento" element={<SeguimientoPrestamo />} />
           <Route path="/costo/calcular" element={<CalcularCosto />} />
--- Microservicios/front-end/src/components/SimularCredito.jsx
+++ Microservicios/front-end/src/components/SimularCredito.jsx
@@ -1,15 +1,15 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import usuarioServices from '../services/usuario.services';
+import simulaServices from '../services/simula.service';
 
-function SimularCredito() {
+function simularCredito() {
   const [credito, setCredito] = useState({
-    rut: '',
     P: '',
     r: '',
     n: '',
-    V: ''
+    V: '',
+    tipo: ''
   });
 
   const [resultado, setResultado] = useState(null);
@@ -27,35 +27,33 @@ function SimularCredito() {
     e.preventDefault();
     setError(null); // Reset error state
     {/*---------------------------------------------------------------------------------------------*/}  
-    const rut = credito.rut;
-    if (rut.length < 7 || rut.length > 12) {
-      setError('ERROR: EL RUT INGRESADO NO ES VÁLIDO. POR FAVOR INGRESAR UN RUT VÁLIDO.');
-      return;
-    }
-    // Verificar que el penúltimo carácter sea '-'
-    if (rut[rut.length - 2] !== '-') {
-      setError('ERROR: EL RUT DEBE CONTENER UN "-" EN EL PENÚLTIMO CARÁCTER.');
-      return;
-    }
-    const { P, r, n, V } = credito;
+    const { P, r, n, V, tipo} = credito;
     if (P < 0 || r < 0 || n < 0 || V < 0) {
       setError('ERROR: LOS VALORES INGRESADOS NO PUEDEN SER NEGARIVOS O IGUAL A 0.');
       return;
     }
-
-    if(n <= 30 && 0.035<= r && r <=0.05 && P <= V*0.8){
+    if (P === '' || r === '' || n === '' || V === '' || tipo === '') {
+      setError('ERROR: DEBE INGRESAR TODOS LOS DATOS.');
+      return;
+    }
+    if (tipo < 1 || tipo > 4) {
+      setError('ERROR: EL TIPO DE CRÉDITO DEBE SER UN VALOR ENTRE 1 Y 4.');
+      return;
+    }
+    {/*---------------------------------------------------------------------------------------------*/}
+    if(n <= 30 && 0.035<= r && r <=0.05 && P <= V*0.8 && tipo == 1){
       alert("PRESTAMO SIMULADO: Primera Vivienda");
       alert("LOS DOCUMENTOS A INGRESAR PARA ESTE TIPO DE PRÉSTAMO SON:\n 1.- Comprobante de ingresos\n 2.- Certificado de avalúo\n 3.- Historial crediticio")
       alert("SIEMPRE RECORDAR: INGRESAR EL INFORME DE DICOM.")
-    }else if(n <= 20 && 0.04<= r && r <=0.06 && P <= V*0.7){
+    }else if(n <= 20 && 0.04<= r && r <=0.06 && P <= V*0.7 && tipo == 2){
       alert("PRESTAMO SIMULADO: Segunda Vivienda");
       alert("LOS DOCUMENTOS A INGRESAR PARA ESTE TIPO DE PRÉSTAMO SON:\n 1.- Comprobante de ingresos\n 2.- Certificado de avalúo\n 3.- Escritura de la primera vivienda\n 4.- Historial crediticio");
       alert("SIEMPRE RECORDAR: INGRESAR EL INFORME DE DICOM.")
-    }else if(n <= 25 && 0.05<= r && r <=0.07 && P <= V*0.6){
+    }else if(n <= 25 && 0.05<= r && r <=0.07 && P <= V*0.6 && tipo == 3){
       alert("PRESTAMO SIMULADO: Propiedades Comerciales");
       alert("LOS DOCUMENTOS A INGRESAR PARA ESTE TIPO DE PRÉSTAMO SON:\n 1.- Estado financiero del negocio\n 2.- Comprobante de ingresos\n 3.- Certificado de avalúo\n 4.- Plan de negocios");
       alert("SIEMPRE RECORDAR: INGRESAR EL INFORME DE DICOM.")
-    }else if(n <=15 && 0.045<= r && r <=0.06 && P<= V*0.5){
+    }else if(n <=15 && 0.045<= r && r <=0.06 && P<= V*0.5 && tipo == 4){
       alert("PRESTAMO SIMULADO: Remodelación");
       alert("LOS DOCUMENTOS A INGRESAR PARA ESTE TIPO DE PRÉSTAMO SON:\n 1.- Comprobante de ingresos\n 2.- Presupuesto de la remodelación\n 3.- Certificado de avalúo actualizado");
       alert("SIEMPRE RECORDAR: INGRESAR EL INFORME DE DICOM.")
@@ -65,7 +63,7 @@ function SimularCredito() {
     }
     {/*---------------------------------------------------------------------------------------------*/}  
     try {      
-      const response = await usuarioServices.calcularMontoMensual(credito);
+      const response = await simulaServices.simularCredito(P, r, n, V, tipo);
       if(response.data === -2) {
         setError('ERROR: EL RUT INGRESADO NO SE ENCUENTRA REGISTRADO EN EL SISTEMA, POR FAVOR INGRESAR UN RUT REGISTRADO O REGISTRARSE EN EL SISTEMA.');
       }else if(response.data === -1) {
@@ -84,13 +82,6 @@ function SimularCredito() {
     <div className="container mt-5">
       <h2>Simular Crédito</h2>
       <form onSubmit={handleSubmit}>
-        <div className="mb-3">
-          <label className="form-label">RUT</label>
-          <input type="text" className="form-control" name="rut" value={credito.rut} onChange={handleChange} placeholder="Ejemplo: 12345678-9" required />
-          <small className="form-text text-muted">
-            Formato: 12345678-9
-          </small>
-        </div>
         {/*---------------------------------------------------------------------------------------------*/}  
         <div className="mb-3">
           <label className="form-label">Monto del Préstamo (P)</label>
@@ -121,13 +112,23 @@ function SimularCredito() {
           <input type="number" className="form-control" name="V" value={credito.V} onChange={handleChange} placeholder="Ejemplo: 100000" required />
           <small className="form-text text-muted">
             Ingrese el valor de la propiedad en pesos.
-          </small>
-          {/*---------------------------------------------------------------------------------------------*/}  
+          </small>  
         </div>
+        {/*---------------------------------------------------------------------------------------------*/}  
+        <div className="mb-3">
+          <label className="form-label">Tipo de Prestamo (tipo)</label>
+          <input type="number" className="form-control" name="tipo" value={credito.tipo} onChange={handleChange} placeholder="1" required />
+
+          <small className="form-text text-muted">
+            Ingrese el valor del tipo de prestamo. Por ejemplo: 1: Primera Vivienda | 2: Segunda Vivienda | 3: Propiedades Comerciales | 4: Remodelación.
+          </small>
+        </div>
+        {/*---------------------------------------------------------------------------------------------*/}
         <div className="d-grid gap-2">
         <button type="submit" className="btn btn-primary btn-lg">Simular</button>
         </div>
       </form>
+      {/*---------------------------------------------------------------------------------------------*/}
       {resultado && (
         <div className="mt-3">
           <h4>Resultado de la Simulación</h4>
@@ -152,4 +153,4 @@ function SimularCredito() {
   );
 }
 
-export default SimularCredito;
+export default simularCredito;
